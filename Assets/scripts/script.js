bb8e9bee20907d2f95cf2ccd5a4af4b68d5bc6b6
mirsaperez <77262010+mirsaperez@users.noreply.github.com>
2021-02-28 15:41:14 -0800

    added lengthselection and isUppercase and isLowercase and isNumbers and isSpecialCharacter

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -18,8 +18,33 @@ let isUppercase = confirm("do you want uppercase")
 // ask user if they want lowercase
 var isLowercase = confirm("do you want lowercase")
 // ask user if they want numbers
+var isNumbers = confirm("do you want numbers")
 // ask user if they want special characters
+var isSpecialCharacters = confirm("do you want special characters")
 // validate the input from user
+  // if lengthSelection is not a number return 
+  if(isNaN(lengthSelection))
+  {
+    return 
+  }
+    // if lengthSelection is less than 8 or greater then 128 return
+    if lengthSelection =(≤ 8 or ≥ 128) let return 
+
+    // if isUppercase + isLowercase + isNumber + isSpecialCharacters = 0 then return
+    if isUppercase + isLowercase + isNumbers +isSpecialCharacters = 0 let return 
+    
+  // create array selection 
+    //if isUppercase then push uppercase array into selection array
+    //if isLowercase then push Lowercase array into selection array
+    //if isNumbers then push Numbers array into selection array
+    //if isSpecialCharacters then push SpecialCharacters array into selection array
+
+  //create new array called password 
+    //start fourloop(i is leess than length selection)
+    //while in loop select random index from selection array
+
+    //return pasword 
+
 // combine all selections into a pool
 // randomized all the selections
 // pulled the length of the password from the pool
@@ -31,16 +56,6 @@ return lengthSelection
 // Add event listener to generate button 
 // when the generate password button is clicked calls the function right password on line 5 
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-// how to make an array 
-// how to assign multiple values to an array (push) look up arraypush
-
 
 
 
